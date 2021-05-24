0c0adec37abd0fded15f79b37a1f2f87e8199a9a
TarynL <tarlytle@gmail.com>
2021-05-24 14:23:16 -0500

    rolling dice complete

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,19 @@
 // Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+const dieRoll = () => {
+    for (let i = 0; i < 10; i++) {
+        let dieOne = Math.floor((Math.random() * 6) + 1);
+        let dieTwo = Math.floor((Math.random() * 6) + 1);
+        if (dieOne === dieTwo) {
+            console.log(`${dieOne} + ${dieTwo} == ${dieOne + dieTwo} DOUBLES!`)
+        }
+        else {
+            console.log(`${dieOne} + ${dieTwo} == ${dieOne + dieTwo}`)
+        };
+    }
+
+}
+
+dieRoll();
